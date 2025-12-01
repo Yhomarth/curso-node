95c36ac9e3c157253865f9cfe974e4168c1137f4
Yhomarth Reyes <y.reyes@simv.gob.do>
2025-12-01 17:13:36 -0400

    Configuración de Typescript en node

---
+++ 03-typescript/src/app.ts
@@ -0,0 +1,18 @@
+
+const heroes = [
+    { id: 1, name: 'Superman', owner: 'DC' },
+    { id: 2, name: 'Batman', owner: 'DC' },
+    { id: 3, name: 'Spiderman', owner: 'Marvel' },
+    { id: 4, name: 'Ironman', owner: 'Marvel' },
+    
+];
+
+
+const findHeroById = (id : number) => {
+    return heroes.find(hero => hero.id === id);
+}
+
+const hero = findHeroById(8);
+
+console.log(hero?.name ?? 'No hero found');
+
