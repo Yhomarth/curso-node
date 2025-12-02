2ea6ed486da13f295fe938ff38ac254053326cf1
Yhomarth Reyes <yhomarth@gmail.com>
2025-12-01 22:13:41 -0400

    Trabajando con node y typescript - complete

--- 03-typescript/src/app.ts
+++ 03-typescript/src/app.ts
@@ -1,18 +1,6 @@
+import { findHeroById } from './services/hero.service';
 
-const heroes = [
-    { id: 1, name: 'Superman', owner: 'DC' },
-    { id: 2, name: 'Batman', owner: 'DC' },
-    { id: 3, name: 'Spiderman', owner: 'Marvel' },
-    { id: 4, name: 'Ironman', owner: 'Marvel' },
-    
-];
-
-
-const findHeroById = (id : number) => {
-    return heroes.find(hero => hero.id === id);
-}
-
-const hero = findHeroById(3);
+const hero = findHeroById(1);
 
 console.log(hero?.name ?? 'No hero found');
 
---
+++ 03-typescript/src/data/heroes.ts
@@ -0,0 +1,14 @@
+
+interface Hero {
+    id: number;
+    name: string;
+    owner: string;
+}
+
+export const heroes : Hero [] = [
+    { id: 1, name: 'Superman', owner: 'DC' },
+    { id: 2, name: 'Batman', owner: 'DC' },
+    { id: 3, name: 'Spiderman', owner: 'Marvel' },
+    { id: 4, name: 'Ironman', owner: 'Marvel' },
+    
+];
